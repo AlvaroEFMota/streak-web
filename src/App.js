fa74482881bb46fcb0444a8b86a57f0df60ce148
Álvaro E. F. Mota <alvaroefmota@gmail.com>
2024-05-28 09:50:51 -0300

    Added a login page

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
+        console.log("useEffect token content:", token);
         if(token) {
             setIsAuthenticated(true);
         }
-    })
+        setIsLoading(false);
+    }, [])
+
+    const handleLogin = (token) => {
+        localStorage.setItem('token', token);
+        setIsAuthenticated(true);
+    }
+
+    if (isLoading) {
+        return <div> Loading... </div>;
+    }
 
     const router = createBrowserRouter([
         {
@@ -33,7 +45,7 @@ function App() {
         },
         {
             path: "/login",
-            element: <Login />,
+            element: <Login onLogin={handleLogin}/>,
         }
     ]);
 
--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,62 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from './Navbar';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
-function Login() {
+function Login({ onLogin}) {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("initialized");
+        fetch('http://127.0.0.1:8080/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data.token); // Exibir o corpo da resposta
+                onLogin(data.token);
+                navigate("/");
+
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+
+    }
 
     return (
         <div>
             <Navbar />
-            <h1>Login page</h1>
+            <form className="loginContainer" onSubmit={handleSubmit}>
+                <div>Login page</div>
+                <div className="inputContainer">
+                    <label htmlFor="email">Email</label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} name="email"/>
+                </div>
+                <div className="inputContainer">
+                    <label htmlFor="password">password</label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} name="password"/>
+                </div>
+                <input className={'inputButton'} type="submit" value={'Log in'} />
+            </form>
         </div>
     );
 }
